515082c0d722a2af84751436b2c7d11a79c88f4d
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-06-22 23:41:05 +0700

    Add vercel analytics

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import classNames from 'classnames'
 import { Header } from '@/components/Header'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={classNames(inter.className)}>
         <Header />
         <div className="max-w-7xl mx-auto py-3 px-4">{children}</div>
+        <SpeedInsights />
       </body>
     </html>
   )

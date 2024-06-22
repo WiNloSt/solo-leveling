b7fe1db2c53b90c11a906d856dfee41a145090e1
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-06-22 17:56:02 +0700

    Use more optimized compression + use E-Tag

--- web/app/api/image/route.ts
+++ web/app/api/image/route.ts
@@ -1,8 +1,10 @@
 import type { NextRequest } from 'next/server'
-import sharp, { type Metadata, type Sharp } from 'sharp'
+import sharp, { type Sharp } from 'sharp'
+import crypto from 'crypto'
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
+  const ifNoneMatch = request.headers.get('If-None-Match')
   const url = searchParams.get('url')
   const quality = Number(searchParams.get('q'))
   const width = Number(searchParams.get('w'))
@@ -30,27 +32,39 @@ export async function GET(request: NextRequest) {
 
   const image = sharp(imageBuffer)
   const metadata = await image.metadata()
-  console.log({ metadata })
 
-  if (metadata.width && metadata.width <= width) {
-    console.log('Image is already smaller than requested width, returning original image.')
-    return new Response(imageBuffer, {
-      status: 200,
-      headers: {
-        'Content-Type': 'image/jpeg',
-        'Cache-Control': `public, max-age=${30 * DAY};`,
-        ...(metadata.size && { 'Content-Length': metadata.size.toString() }),
-      },
-    })
+  // if (metadata.width && metadata.width <= width) {
+  //   console.log('Image is already smaller than requested width, returning original image.')
+  //   return new Response(imageBuffer, {
+  //     status: 200,
+  //     headers: {
+  //       'Content-Type': 'image/jpeg',
+  //       'Cache-Control': `public, max-age=${30 * DAY};`,
+  //       ...(metadata.size && { 'Content-Length': metadata.size.toString() }),
+  //     },
+  //   })
+  // }
+
+  const DEFAULT_MAX_IMAGE_WIDTH = 720
+  const defaultWidth = metadata.width || DEFAULT_MAX_IMAGE_WIDTH
+  const { data, info } = await processImage(image, {
+    width: width <= defaultWidth ? width : defaultWidth,
+    quality,
+  })
+
+  const eTag = getHash(data)
+  console.log({ original: metadata, resized: info, ifNoneMatch, eTag })
+  if (ifNoneMatch === eTag) {
+    return new Response(null, { status: 304, statusText: 'Not Modified' })
   }
-  const { data, info } = await processImage(image, { width, quality })
-  console.log('Resizing image.')
+
   return new Response(data, {
     status: 200,
     headers: {
-      'Content-Type': 'image/jpeg',
+      'Content-Type': 'image/webp',
       'Cache-Control': `public, max-age=${30 * DAY};`,
       'Content-Length': info.size.toString(),
+      ETag: eTag,
     },
   })
 }
@@ -58,5 +72,11 @@ export async function GET(request: NextRequest) {
 const DAY = 24 * 60 * 60
 
 async function processImage(image: Sharp, { quality, width }: { quality: number; width: number }) {
-  return image.resize({ width }).jpeg({ quality: quality }).toBuffer({ resolveWithObject: true })
+  return image.resize({ width }).webp({ quality }).toBuffer({ resolveWithObject: true })
+}
+
+function getHash(buffer: Buffer) {
+  const hash = crypto.createHash('sha1')
+  hash.update(buffer)
+  return hash.digest('hex')
 }

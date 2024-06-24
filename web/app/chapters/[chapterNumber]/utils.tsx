cf501ddc2d642c0cf3d8ca70c93beec86403eec7
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-06-24 18:53:05 +0700

    Improve cache hit

--- web/app/chapters/[chapterNumber]/utils.tsx
+++ web/app/chapters/[chapterNumber]/utils.tsx
@@ -1,8 +1,11 @@
 import { type ImageLoaderProps } from 'next/image'
 import type { ChapterPage } from './page'
 
+export const MAX_IMAGE_WIDTH = 720
+
 export function createImageUrl({ src, width, quality }: ImageLoaderProps) {
-  return `/api/image?url=${src}&w=${width}&q=${quality}`
+  const normalizedWidth = width <= MAX_IMAGE_WIDTH ? width : MAX_IMAGE_WIDTH
+  return `/api/image?url=${src}&w=${normalizedWidth}&q=${quality}`
 }
 
 export async function loadPages(chapterNumber: number | string): Promise<ChapterPage[]> {

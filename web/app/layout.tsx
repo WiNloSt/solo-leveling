4db8d619e379366f760034f52ff9ff36f5f9c941
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-06-27 01:29:27 +0700

    Add google analytics

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -5,6 +5,7 @@ import classNames from 'classnames'
 import { Header } from '@/components/Header'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/react'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         <div className="max-w-7xl mx-auto py-3 px-4">{children}</div>
         <SpeedInsights />
         <Analytics />
+        <GoogleAnalytics gaId="G-1X4YG5K79Q" />
       </body>
     </html>
   )
